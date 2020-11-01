9dd1021bf2664f525572c520bad8c13a7d96f151
Bdvx <bdv2507@gmail.com>
2020-11-02 01:16:12 +0300

    fix: headers and productId

--- product-service/src/getAllProducts.js
+++ product-service/src/getAllProducts.js
@@ -4,6 +4,7 @@ export const getAllProducts = async (event) => {
   console.log('getAllProducts lambda called with event: ', event);
 
   return {
+    headers: { "Access-Control-Allow-Origin": "*" },
     statusCode: 200,
     body: JSON.stringify(productList)
   };
--- product-service/src/getProductById.js
+++ product-service/src/getProductById.js
@@ -1,12 +1,25 @@
 import productList from './productList.json';
 
+const headers = { "Access-Control-Allow-Origin": "*" };
+
 export const getProductById = async (event) => {
   console.log('getProductById lambda called with event: ', event);
   const { productId } = event.pathParameters;
+  // search needed product - by id field
+  const product = productList.find((prod) => prod.id === productId);
+
+  if (product === -1){
+    return {
+        headers,
+        statusCode: 404,
+        body: "Target product was not found",
+      };
+  }
 
   return {
+    headers,
     statusCode: 200,
-    body: JSON.stringify(productList[productId])
+    body: JSON.stringify(product),
   };
 
 };
