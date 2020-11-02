a8dfb9bbf3d2d0f4be9c5d22f541666b2e556719
Bdvx <bdv2507@gmail.com>
2020-11-02 03:12:40 +0300

    feat: add async/await and change default hardcoded data

--- product-service/src/getAllProducts.js
+++ product-service/src/getAllProducts.js
@@ -3,6 +3,10 @@ import productList from './productList.json';
 export const getAllProducts = async (event) => {
   console.log('getAllProducts lambda called with event: ', event);
 
+  // emulate delay from remote URL fetch
+  // suppose we'll get real URL here and not file with hardcoded values
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
   return {
     headers: { "Access-Control-Allow-Origin": "*" },
     statusCode: 200,
--- product-service/src/getProductById.js
+++ product-service/src/getProductById.js
@@ -5,6 +5,11 @@ const headers = { "Access-Control-Allow-Origin": "*" };
 export const getProductById = async (event) => {
   console.log('getProductById lambda called with event: ', event);
   const { productId } = event.pathParameters;
+
+  // emulate delay from remote URL fetch
+  // suppose we'll get real URL here and not file with hardcoded values
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
   // search needed product - by id field
   const product = productList.find((prod) => prod.id === productId);
 
