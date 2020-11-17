6ce94a65196038c8bfbdc9f0465c7d1a7153c13d
Denis_Beskorovainyi <besdenv@gmail.com>
2020-11-17 05:14:00 +0300

    feat: importFileParser lambda

--- import-service/src/importProductsFile.js
+++ import-service/src/importProductsFile.js
@@ -5,18 +5,19 @@ import { headers, errMessage } from './helpers.js';
 export const importProductsFile = async (event) => {
     console.log('importProductsFile lambda called with event: ', event);
 
-    const { BUCKET: Bucket } = process.env;
-    const { name } = event.queryStringParameters || {};
+    const { BUCKET: Bucket, REGION: region } = process.env;
+    const { name, contentType: ContentType } = event.queryStringParameters || {};
 
     const params = {
         Bucket,
         Key: `uploaded/${name}`,
+        Expires: 60,
         ContentType,
     }
     try {
-        const s3 = new AWS.S3({region: 'eu-west-1', signatureVersion: 'v4'});
+        const s3 = new AWS.S3({region, signatureVersion: 'v4'});
         
-        const signedURL = s3.getSignedUrlPromise('putObject', params);
+        const signedURL = await s3.getSignedUrlPromise('putObject', params);
         return {
           statusCode: 200,
           headers,
